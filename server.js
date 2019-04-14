13523a2bdf4cd4fe111e867672659c9681885b9d
Veda Anderson <vedaanderson@Vedas-MacBook-Pro.local>
2019-04-14 17:10:01 -0600

    Commit 4 - show view shows pokemon details

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(bodyParser.json());
 
 
 const pokemon = require('./pokemon');
@@ -13,8 +13,19 @@ const pokemon = require('./pokemon');
 app.get('/pokemon', function(req, res) {
     res.send(pokemon);
   });
-  
+// index route
+ app.get('/pokemon', function(req, res){
+    res.render('index.js', {
+        "pokemon": pokemon 
+    });
+ });  
 
+ // show route
+app.get('/pokemon/:id', function(req, res) {
+    res.render('show.ejs', {
+      "pokemon" : pokemon[req.params.id]
+   });
+  });
 
 
 
