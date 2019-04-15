20e5a6bbf6a510ab24e57f09c9eee26c31ce8c3b
Veda Anderson <vedaanderson@Vedas-MacBook-Pro.local>
2019-04-15 17:50:49 -0600

    Crud is complete -Will style now

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const methodOverride = require('method-override');
 
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(methodOverride('_method'));
+app.use(express.static('public'));
+// app.use(express.static('public'))
 
 
 const Pokemon = require('./pokemon');
@@ -15,15 +19,45 @@ app.get('/pokemon',(req, res) => {
     "pokemon": Pokemon
   });
 });
+app.get('/pokemon', (req, res)=>{
+  res.render('new.ejs')
+});
 
+app.post('/pokemon', (req, res)=>{
+  Pokemon.push(req.body)
+  res.redirect('/pokemon')
+});
+
+
+app.get('/pokemon/:id/edit', (req, res)=>{
+  res.render('edit.ejs', {
+      "pokemon": Pokemon[req.params.id],
+      id: req.params.id
+  });
+});
+
+app.get('/pokemon/:id', (req, res)=>{
+  res.render('show.ejs', {
+      "pokemon": Pokemon[req.params.id],
+      id: req.params.id 
+  });
+});
  // show route
 app.get('/pokemon/:id', (req, res) => {
     res.render('show.ejs', {
       "pokemon" : Pokemon[req.params.id]
    });
   });
-  
 
+  app.put('/pokemon/:id', (req, res)=>{
+    Pokemon[req.params.id] = req.body // instead of pushing we are assigning 
+    res.redirect('/pokemon')
+});
+
+app.delete('/pokemon/:id', (req, res)=>{
+  Pokemon.splice(req.params.id, 1)
+  res.redirect('/pokemon')
+});
 
 
 
